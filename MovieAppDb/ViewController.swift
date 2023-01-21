b3851e420557f0d1f477395d0816c94510d38ec7
Sait Cihaner <scihaner@partner.innova.com.tr>
2023-01-21 15:08:02 +0300

    Initial Commit

---
+++ MovieAppDb/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MovieAppDb
+//
+//  Created by Sait Cihaner on 21.01.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
